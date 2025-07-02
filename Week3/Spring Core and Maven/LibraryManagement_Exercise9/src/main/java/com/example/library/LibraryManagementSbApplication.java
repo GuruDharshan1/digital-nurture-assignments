4c19d28827f43410aec5dff3ee7822ebc87a10e2
GuruDharshan1 <118243411+GuruDharshan1@users.noreply.github.com>
2025-07-02 22:09:56 +0530

    Add files via upload

---
+++ Week3/Spring Core and Maven/LibraryManagement_Exercise9/src/main/java/com/example/library/LibraryManagementSbApplication.java
@@ -0,0 +1,12 @@
+package com.example.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementSbApplication.class, args);
+    }
+}
