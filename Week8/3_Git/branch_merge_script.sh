fe0025ca46311b6a97bdecfc3c2fb4d9b2189cce
GuruDharshan1 <118243411+GuruDharshan1@users.noreply.github.com>
2025-08-08 19:17:10 +0530

    Add files via upload

---
+++ Week8/3_Git/branch_merge_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Branching
+git checkout -b GitNewBranch
+git branch -a
+touch branchfile.txt
+echo "This is a file in GitNewBranch" > branchfile.txt
+git add branchfile.txt
+git commit -m "Added branchfile.txt in GitNewBranch"
+git status
+
+# Merging
+git checkout master
+git diff GitNewBranch
+# Note: Use P4Merge tool manually for visual diff
+
+git merge GitNewBranch
+git log --oneline --graph --decorate
+
+# Optional: delete the branch
+git branch -d GitNewBranch
+git status
